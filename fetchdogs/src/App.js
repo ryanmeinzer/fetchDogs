f41e1b3bbf6ec7291381ab5579bed2d3c2b9060d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-03-16 22:22:27 -0700

    first commit with all fetches done properly with class component

---
+++ fetchdogs/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import FetchWithClassComponent from './FetchWithClassComponent'
+
+function App() {
+  return (
+    <>
+      <h1>Fetch Dogs</h1>
+      <FetchWithClassComponent />
+    </>
+  )
+}
+
+export default App
---
+++ fetchdogs/src/FetchWithClassComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class FetchWithClassComponent extends Component {
+
+    state = {
+        titles: [],
+        imagesOriginalUrls: []
+    }
+
+    componentDidMount() {
+        fetch('https://api.giphy.com/v1/gifs/search?q=dogs&api_key=cwxQISXRJJ5EO5nAW0kfohP1RSmK2PSE&rating=g')
+            .then(response => response.json())
+            .then(json =>
+                this.setState({
+                    // state: json.data
+                    titles: json.data.map(element => element.title),
+                    imagesOriginalUrls: json.data.map(element => element.images.original.url)
+            })
+        )
+    }
+    render() {
+        return (
+            <>
+                {/* {this.state.titles.map(element => <div>{element.title}</div>)} */}
+                {this.state.titles.map(element => <div>{element}</div>)}
+                {this.state.imagesOriginalUrls.map(element => <img key={element} src={`${element}`} alt="loading" />)}
+            </>
+        )
+    }
+}
+
+export default FetchWithClassComponent
