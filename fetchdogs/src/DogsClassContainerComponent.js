01e3a73a2dd443725899e1f8c651624cbf4e5140
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-03-27 13:25:55 -0700

    added loading function

--- fetchdogs/src/DogsClassContainerComponent.js
+++ fetchdogs/src/DogsClassContainerComponent.js
@@ -4,24 +4,37 @@ import DogsFunctionalPresentationalParentComponent from './DogsFunctionalPresent
 // fetches data with lifecycle method and handles state for all child components
 class DogsClassContainerComponent extends Component {
     state = {
-        dogs: []
+        dogs: [],
+        loading: false
     }
 
     componentDidMount() {
+        // this.setState({loading: true})
         // fetch('./data.json')
         fetch('https://api.giphy.com/v1/gifs/search?q=dogs&api_key=cwxQISXRJJ5EO5nAW0kfohP1RSmK2PSE&rating=g')
             .then(response => response.json())
             .then(json =>
                 this.setState({
-                    dogs: json.data
+                    dogs: json.data,
+                    loading: false
                 })
             )
     }
 
+    // handle fetch upon app load with loading message if still loading
+    // handleLoading = () => {
+    //     if (this.state.loading) {
+    //         return <div>Loading...</div>
+    //     } else {
+    //         return <div>{Object.values(this.state.dogs[0])[0]}</div>
+    //     }
+    // }
+
     render() {
         return (
-            // for this to work, state cannot initally be null as render will occur before ComponentDidMount
+            // for either of these to work, state cannot initally be null as render will occur before ComponentDidMount
             // <>
+            //     {this.handleLoading()}
             //     {Object.values(this.state.dogs[0])[0]}
             // </>
             <DogsFunctionalPresentationalParentComponent dogs={this.state.dogs}/>
